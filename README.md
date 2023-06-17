836b581d84e638f7064ede4fd606ad31022a31ed
Julian <thiele.juli@gmail.com>
2023-06-17 13:42:10 +0200

    Added popup, icons, readme and comments

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # chess-opponent-insights
+
+> **Warning** <br>
+> This addon is still in development and bugs might occur. If that happens, reload the site and it should work again. <br>
+
+## 🚀 How it works
+
+This extension works only on chrome based browsers and is only for chess.com at the moment. It shows you the stats of your opponent in the current game. 
+
+- First three numbers are the wins/loses/draws of the player
+- Percentage in brackets is the average accuracy of the player
+  - Accuracy is only available on games which were reveiwed
+
+![preview](preview.png)
+
+## ⚙️ Installation
+
+Chrome Web Store link coming soon
+
+## 🪲 Known bugs
+
+- If you flip the board, the stats don't flip with it.
--- chess-opponent-insights.js
+++ chess-opponent-insights.js
@@ -1,4 +1,8 @@
-async function execute() {
+/**
+ * This function is called when a chess.com tab is loaded.
+ * It gets data from the public chess.com API and updates the DOM.
+ */
+function update_stats() {
   // get both chess players from the DOM
   const player_top_el = document.querySelector(
     ".board-layout-top .user-tagline-username"
@@ -16,10 +20,6 @@ async function execute() {
   if (flag_top) flag_top.remove();
   if (flag_bottom) flag_bottom.remove();
 
-  // get parent elements
-  let el_top = player_top_el.querySelector(".user-tagline-rating");
-  let el_bottom = player_bottom_el.querySelector(".user-tagline-rating");
-
   // create new elements and append them to the DOM
   el_top = document.createElement("div");
   el_top.classList.add("user-tagline-rating", "flag-top");
@@ -31,35 +31,42 @@ async function execute() {
   el_bottom.style.marginLeft = "10px";
   player_bottom_el.parentElement.appendChild(el_bottom);
 
-  el_top.innerText = "Loading...";
-  el_bottom.innerText = "Loading...";
+  // el_top.innerText = "Updating...";
+  // el_bottom.innerText = "Updating...";
 
-  // request data from chess.com public API
-  let stats_top, stats_bottom;
-  try {
-    stats_top = await get_chess_data(player_top_el.innerText);
-    stats_bottom = await get_chess_data(player_bottom_el.innerText);
-  } catch (err) {
-    // if error occurs, remove elements from DOM and execute again
-    el_top.remove();
-    el_bottom.remove();
-    execute();
-    return;
-  }
+  // get stats for players and update ui elements
+  // if error occurs, remove elements from DOM and update again
+  get_chess_data(player_top_el.innerText)
+    .then((data) => update_element(el_top, data))
+    .catch(() => {
+      el_top.remove();
+      update_stats();
+    });
 
-  // update the stats in the DOM
-  let str_top = `${stats_top.wld.wins}/${stats_top.wld.loses}/${stats_top.wld.draws}`;
-  let str_bottom = `${stats_bottom.wld.wins}/${stats_bottom.wld.loses}/${stats_bottom.wld.draws}`;
-
-  // if the accuracy is available, add it to the string
-  if (stats_top.accuracy.avg !== 0) str_top += ` (${stats_top.accuracy.avg}%)`;
-  if (stats_bottom.accuracy.avg !== 0)
-    str_bottom += ` (${stats_bottom.accuracy.avg}%)`;
-
-  el_top.innerText = str_top;
-  el_bottom.innerText = str_bottom;
+  get_chess_data(player_bottom_el.innerText)
+    .then((data) => update_element(el_bottom, data))
+    .catch(() => {
+      el_bottom.remove();
+      update_stats();
+    });
 }
 
+/**
+ * Updates an element with the provided stats.
+ * @param {HTMLElement} el - The element to update.
+ * @param {Object} stats - The stats data to display.
+ */
+function update_element(el, stats) {
+  let str = `${stats.wld.wins}/${stats.wld.loses}/${stats.wld.draws}`;
+  if (stats.accuracy.avg !== 0) str += ` (${stats.accuracy.avg}%)`;
+  el.innerText = str;
+}
+
+/**
+ * Fetches chess data for the given username from the chess.com public API.
+ * @param {string} username - The username to fetch data for.
+ * @returns {Promise<Object>} - The fetched chess data.
+ */
 async function get_chess_data(username) {
   // prepare date
   const date = new Date();
@@ -72,12 +79,21 @@ async function get_chess_data(username) {
   const response = await fetch(url);
   const data = await response.json();
 
+  // get settings from storage
+  const settings = await getSettingsFromStorage();
+
   // filter and get stats
-  const games_filtered = filter_games(data.games);
+  const games_filtered = await filter_games(data.games, settings);
   const stats = get_stats(games_filtered, username);
   return stats;
 }
 
+/**
+ * Calculates and returns the statistics for the given games and username.
+ * @param {Array} games - The games data to calculate stats from.
+ * @param {string} username - The username to calculate stats for.
+ * @returns {Object} - The calculated statistics.
+ */
 function get_stats(games, username) {
   // count wld stats and sum up accuracy
   let accuracy_sum = 0;
@@ -121,9 +137,19 @@ function get_stats(games, username) {
   return stats;
 }
 
-function filter_games(games) {
-  const game_modes = ["rapid", "blitz", "bullet"];
-  const max_games = 20;
+/**
+ * Filters the games based on the settings and returns the filtered games.
+ * @param {Array} games - The games data to filter.
+ * @param {Object} settings - The settings to apply for filtering.
+ * @returns {Array} - The filtered games.
+ */
+function filter_games(games, settings) {
+  // set defaults if settings are not available
+  if (!settings.max_games || !settings.game_modes)
+    settings = {
+      max_games: 20,
+      game_modes: ["blitz", "rapid", "bullet"],
+    };
 
   // Sort games by end time in descending order
   games.sort((a, b) => b.end_time - a.end_time);
@@ -132,13 +158,19 @@ function filter_games(games) {
   return games
     .filter((game) => {
       return (
-        (Array.isArray(game_modes) && game_modes.includes(game.time_class)) ||
-        game.time_class === game_modes
+        (Array.isArray(settings.game_modes) &&
+          settings.game_modes.includes(game.time_class)) ||
+        game.time_class === settings.game_modes
       );
     })
-    .slice(0, max_games);
+    .slice(0, settings.max_games);
 }
 
+/**
+ * Transforms the given result into a simplified result.
+ * @param {string} result - The result to transform.
+ * @returns {string} - The transformed result.
+ */
 function transform_result(result) {
   let r = "";
   switch (result) {
@@ -167,5 +199,19 @@ function transform_result(result) {
   return r;
 }
 
-// initial run after 2.2 seconds to make sure the elements are loaded
-setTimeout(execute, 2200);
+/**
+ * Retrieves the settings from the storage.
+ * @returns {Promise<Object>} - The settings data.
+ */
+async function getSettingsFromStorage() {
+  return new Promise((resolve) => {
+    chrome.storage.local.get(["coi_settings"], (result) => {
+      let coi_settings = {};
+      if (result.coi_settings) coi_settings = result.coi_settings;
+      resolve(coi_settings);
+    });
+  });
+}
+
+// initial run after 2 seconds to make sure the elements are loaded
+setTimeout(update_stats, 2000);
