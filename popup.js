35d315b55340cc20a8fd53ac15b2a09682c84a80
Julian 41455100+thieleju@users.noreply.github.com <thiele.juli@gmail.com>
2023-06-18 03:49:00 +0200

    Refactored functions and set to time_interval to last 12 hours

--- popup.js
+++ popup.js
@@ -1,20 +1,20 @@
 async function initializePopup() {
   // read settings from storage
-  let coi_settings = await getSettingsFromStorage();
+  let settings = await getSettingsFromStorage();
 
   // show settings in popup
-  document.getElementById("games_max").textContent = coi_settings.games_max;
-  document.getElementById("game_modes").textContent = coi_settings.game_modes;
+  // document.getElementById("games_max").textContent = settings.games_max;
+  document.getElementById("game_modes").textContent = settings.game_modes;
 }
 
 document.addEventListener("DOMContentLoaded", initializePopup);
 
 async function getSettingsFromStorage() {
   return new Promise((resolve) => {
-    chrome.storage.local.get(["coi_settings"], (result) => {
-      let coi_settings = {};
-      if (result.coi_settings) coi_settings = result.coi_settings;
-      resolve(coi_settings);
+    chrome.storage.local.get(["settings"], (result) => {
+      let settings = {};
+      if (result.settings) settings = result.settings;
+      resolve(settings);
     });
   });
 }
