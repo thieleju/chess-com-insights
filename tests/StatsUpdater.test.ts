d15a59a21db1afbbc09ae19c16d7550e9adfa3f6
Julian 41455100+thieleju@users.noreply.github.com <thiele.juli@gmail.com>
2023-08-07 19:54:04 +0200

    Add mocha tests and nyc coverage

---
+++ tests/StatsUpdater.test.ts
@@ -0,0 +1,10 @@
+// import { describe, it, beforeEach } from "mocha"
+import { expect } from "chai"
+// import { StatsUpdater } from "../src/modules/StatsUpdater"
+// import sinon from "sinon"
+
+describe("test", () => {
+  it("should work", () => {
+    expect(true).to.equal(true)
+  })
+})
